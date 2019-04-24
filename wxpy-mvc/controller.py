d15b617e5249c50733108b26f978c18eb9544691
Xett <3954937+Xett@users.noreply.github.com>
2019-04-25 03:45:10 +1000

    Controller
    add model and view as children, establish mainloop

--- wxpy-mvc/controller.py
+++ wxpy-mvc/controller.py
@@ -1,4 +1,11 @@
 import wx
 class Controller:
-    def __init__(self):
+    def __init__(self, model, view):
         self.App=wx.App()
+        self.Model=model
+        self.View=view
+        self.running=False
+    def main(self):
+        self.running=True
+        while self.running:
+            self.App.MainLoop()
