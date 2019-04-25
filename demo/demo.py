58de69ee92ed477304295d15e0b2d104ccca1432
Xett <3954937+Xett@users.noreply.github.com>
2019-04-25 16:39:41 +1000

    fix imports

--- demo/demo.py
+++ demo/demo.py
@@ -1,8 +1,8 @@
 import wx
 import wxpy-mvc
-from wxpy-mvc.model import Model
-from wxpy-mvc.view import View
-from wxpy-mvc.controller import Controller
+from wxpy-mvc import model.Model as Model
+from wxpy-mvc import view.View as View
+from wxpy-mvc import controller.Controller as Controller
 if __name__=="__main__":
     m=Model()
     v=View('a')
