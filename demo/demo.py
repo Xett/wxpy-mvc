019fc404ebbd5d0cf34ad0e004a6f6100e9821e2
Xett <3954937+Xett@users.noreply.github.com>
2019-04-25 16:41:12 +1000

    fix improts

--- demo/demo.py
+++ demo/demo.py
@@ -1,9 +1,6 @@
 import wx
 import wxpy-mvc
-from wxpy-mvc import model.Model as Model
-from wxpy-mvc import view.View as View
-from wxpy-mvc import controller.Controller as Controller
 if __name__=="__main__":
-    m=Model()
-    v=View('a')
-    c=Controller(m, v)
+    m=wxpy-mvc.model.Model()
+    v=wxpy-mvc.view.View('a')
+    c=wxpy-mvc.controller.Controller(m, v)
